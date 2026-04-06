c27eaf47f4e7644608470c61b8dc1557f3ee7987
Praveen <your-email@gmail.com>
2026-04-06 11:01:16 +0530

    fix api url

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://portfolio-backend-44a9.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
